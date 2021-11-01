bdefb18d650a6a1b483e305277a11b0c2457aad2
Xsop-sop <songjy08@naver.com>
2021-11-01 22:58:05 +0900

    템플릿 문자열 (`)

---
+++ 02. 알아두어야 할 자바스크립트/03. 템플릿 문자열.js
@@ -0,0 +1,11 @@
+const num1 = 1;
+const num2 = 2;
+const result = 3;
+const string1 = num1 + ' 더하기 ' + num2 + '는 \'' + result + '\'';
+console.log(string1);
+
+const num3 = 1;
+const num4 = 2;
+const result2 = 3;
+const string2  = `${num3} 더하기 ${num4}는 '${result2}'`;
+console.log(string2);
